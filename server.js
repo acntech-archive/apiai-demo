f34256400d737977ca6726cc001c9a78c6d6d46f
Simon Litlehamar <simon@litlehamar.net>
2016-09-27 18:22:34 +0200

    Added boilerplate code for simple REST API

---
+++ server.js
@@ -0,0 +1,27 @@
+
+var http = require('http');
+var path = require('path');
+
+var express = require('express');
+var bodyParser = require('body-parser')
+
+var app = express();
+var server = http.createServer(app);
+
+app.use(bodyParser.json())
+
+app.post('/webhook', function(req, res) {
+  res.sendStatus(200);
+});
+
+app.get('/', function(req, res) {
+  res.send('Welcome to the API.ai demo API');
+});
+
+
+server.listen(process.env.PORT || 3000, process.env.IP || "0.0.0.0", function(){
+  var addr = server.address();
+  console.log("Server listening at", addr.address + ":" + addr.port);
+});
+
+
